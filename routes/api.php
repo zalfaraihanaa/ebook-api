e7d22068689db6bc155d463d8d7f174eb49ff708
zalfaraihanaa <98083027+zalfaraihanaa@users.noreply.github.com>
2022-10-26 16:17:49 +0700

    modul 2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get ('me', [AuthController::class, 'me']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']); 
+Route::delete('/books/{id}', [BookController::class, 'destroy']);
